e3a8551f9cde40ede88ec416aebb3f436bd49b27
ErBlack <ErBlack@live.ru>
2018-04-07 18:40:40 +0300

    user

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const Light = require('./light');
-const onDomReady = require('./ondomready'); 
-
-require('../style.css');
-
-onDomReady(function() {
-    new Light(document.querySelector('#light'));
-});
